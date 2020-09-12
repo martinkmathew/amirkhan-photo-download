df390e1330b231abee944c80d6e2391bae5bf1cf
martinkmathew <49126334+martinkmathew@users.noreply.github.com>
2020-09-12 14:13:00 +0530

    Update photodownloader.py

--- photodownloader.py
+++ photodownloader.py
@@ -6,18 +6,6 @@ page = urlopen('https://www.indiaglitz.com/aamir-khan-photos-hindi-actor-2628998
 soup = BeautifulSoup(urlopen('https://www.indiaglitz.com/aamir-khan-photos-hindi-actor-2628998-7950'))
 counter = 0
 
-#directory = "amir"
-
-# Parent Directory path
-#parent_dir = "C:/Users\justi\.PyCharmCE2019.2\config\scratches"
-
-# Path
-#path = os.path.join(parent_dir, directory)
-
-
-#os.mkdir(path)
-#print("Directory '% s' created" % directory)
-
 
 for img in soup.find_all(title="Aamir Khan"):
     with open("image" + str(counter)+".jpg",'wb') as f:
