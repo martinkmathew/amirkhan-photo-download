772cc57898110b32637da0a8cb6c6afcbc011e8c
martinkmathew <49126334+martinkmathew@users.noreply.github.com>
2020-09-13 12:47:58 +0530

    Update photodownloader.py

--- photodownloader.py
+++ photodownloader.py
@@ -2,14 +2,20 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import os
 
+dir = "aamir khan images"
+loc_for_folder = r"C:\Users\justi\downloads"
+path = os.path.join(loc_for_folder, dir)
+os.mkdir(path)
+os.chdir(path)
+
 page = urlopen('https://www.indiaglitz.com/aamir-khan-photos-hindi-actor-2628998-7950')
 soup = BeautifulSoup(urlopen('https://www.indiaglitz.com/aamir-khan-photos-hindi-actor-2628998-7950'))
 counter = 0
 
 
-for img in soup.find_all(title="Aamir Khan"):
-    with open("image" + str(counter)+".jpg",'wb') as f:
-        m=f.write(urlopen(img['src']).read())
+for img in soup.find_all(title="Aamir Khan"):    #since the images of amir will be given title ,we can find out by inspecting in the page
+    with open("image" + str(counter)+".jpg",'wb') as f:   
+        f.write(urlopen(img['src']).read())
     counter += 1
 
 
